a9862809230e2e006b1d96cdc207f5ada5ac5305
JJ Teoh <thewebdev.jjteoh@gmail.com>
2020-03-24 14:39:52 +0800

    fix router redirecting issuu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,30 @@ import { UserService } from './services/user/user.service';
 export class AppComponent {
   constructor(private userService: UserService, private auth: AuthService, router: Router){
     auth.user$.subscribe(user => {
-      if(user){
-        // we save everytimes login because its login using google
+     if(!user) return;
+
+     // we save everytimes login because its login using google
         // user may change his/her displayname on google
-        userService.save(user);
+     userService.save(user);
+     let returnUrl = localStorage.getItem('returnUrl'); 
+     
+     if(!returnUrl) return;
+    
+     returnUrl = returnUrl == "null"?  '' : returnUrl;
+     localStorage.removeItem('returnUrl');
+     router.navigateByUrl(returnUrl);
 
-        let returnUrl = localStorage.getItem('returnUrl');
-        if(!returnUrl || returnUrl == "null")
-          returnUrl = 'home';
+      // if(user){
+      //   // we save everytimes login because its login using google
+      //   // user may change his/her displayname on google
+      //   userService.save(user);
 
-        router.navigateByUrl(returnUrl);
-      }
+      //   let returnUrl = localStorage.getItem('returnUrl');
+      //   if(!returnUrl || returnUrl == "null")
+      //     returnUrl = 'home';
+
+      //   router.navigateByUrl(returnUrl);
+      // }
     });
   }
 }
